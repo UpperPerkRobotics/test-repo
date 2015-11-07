e105debf81e0b18280c80b34e965396688514506
Bob Foreman <foreman3@hotmail.com>
2015-11-07 12:40:12 -0500

    New files present, testing a new algorithm + Filter.c file + filterTest.c file + simpleTestCounter.c file + created new code in Filter.c and filterTest.c to print to find the average of numbers. This is planned to be used to equalize the power and speed of the motors/launcher wheel more efficiently. - Nothing removed = Nothing changed in main program task file (only changed code in Filter.c and filterTest.c)

---
+++ Filter.c
@@ -0,0 +1,41 @@
+int rightValues[5];
+
+int doFilter(int * valueList) {
+	// get total
+	int total = 0;
+
+
+	int biggest = valueList[0];
+	int lowest = valueList[0];
+
+
+	for(int i = 0; i < 5; i++){
+			total += valueList[i];
+
+			if(valueList[i] > biggest){
+				biggest = valueList[i];
+			}
+
+			if(valueList[i] < lowest){
+				lowest = valueList[i];
+			}
+
+	}
+int filteredTotal = total - (biggest + lowest);
+int filterAverage = (filteredTotal/3);
+
+return filterAverage;
+
+}
+
+
+int filterRight(int value){
+	// maintain list
+	for(int i = 4; i > 0; --i){
+			rightValues[i] = rightValues[i-1];
+	}
+
+	rightValues[0] = value;
+	// get total
+	return doFilter(rightValues);
+}
