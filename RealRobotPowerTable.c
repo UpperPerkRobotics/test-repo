8d1fed2020ba656bcc2128d6bcf8b8b0cd15155a
sciroppo <foremana@students.upsd.org>
2015-12-19 11:08:45 -0500

    Fixed the values in the RobotPowerTable.

--- RealRobotPowerTable.c
+++ RealRobotPowerTable.c
@@ -18,9 +18,9 @@ int getRightShooterPower(int speed){
 	else if (speed > 1200){
 		power = 76;
 	}
-	else if (speed > 1175)
+	else if (speed > 1175){
 		power = 75;
-}
+	}
 	else if (speed > 1166){
 		power = 74;
 	}
@@ -57,7 +57,7 @@ int getRightShooterPower(int speed){
 	else if (speed > 966){
 		power = 62;
 	}
-	else if (spped > 950){
+	else if (speed > 950){
 		power = 61;
 	}
 	else if (speed > 933){
@@ -66,55 +66,55 @@ int getRightShooterPower(int speed){
 	else if (speed > 900){
 		power = 58;
 	}
-		else if (spped > 875){
+		else if (speed > 875){
 		power = 57;
 	}
 	else if (speed > 866){
 		power = 56;
 	}
-		else if (spped > 845){
+		else if (speed > 845){
 		power = 54;
 	}
 	else if (speed > 833){
 		power = 52;
 	}
-		else if (spped > 815){
+		else if (speed > 815){
 		power = 51;
 	}
 	else if (speed > 800){
 		power = 50;
 	}
-		else if (spped > 775){
+		else if (speed > 775){
 		power = 49;
 	}
 	else if (speed > 766){
 		power = 48;
 	}
-		else if (spped > 745){
+		else if (speed > 745){
 		power = 47;
 	}
 	else if (speed > 733){
 		power = 46;
 	}
-		else if (spped > 715){
+		else if (speed > 715){
 		power = 45;
 	}
 	else if (speed > 700){
 		power = 44;
 	}
-		else if (spped > 680){
+		else if (speed > 680){
 		power = 43;
 	}
 	else if (speed > 666){
 		power = 42;
 	}
-		else if (spped > 650){
+		else if (speed > 650){
 		power = 41;
 	}
 	else if (speed > 633){
 		power = 40;
 	}
-		else if (spped > 615){
+		else if (speed > 615){
 		power = 39;
 	}
 	else if (speed > 600){
@@ -135,127 +135,127 @@ int getLeftShooterPower(int speed){
 	if (speed > 1300){
 		power = 88;
 	}
-		else if (spped > 1275){
+		else if (speed > 1275){
 		power = 87;
 	}
 	else if (speed > 1266){
 		power = 86;
 	}
-		else if (spped > 1245){
+		else if (speed > 1245){
 		power = 85;
 	}
 	else if (speed > 1233){
 		power = 84;
 	}
-		else if (spped > 1215){
+		else if (speed > 1215){
 		power = 83;
 	}
 	else if (speed > 1200){
 		power = 82;
 	}
-		else if (spped > 1175){
+		else if (speed > 1175){
 		power = 81;
 	}
 	else if (speed > 1166){
 		power = 80;
 	}
-		else if (spped > 1150){
+		else if (speed > 1150){
 		power = 79;
 	}
 	else if (speed > 1133){
 		power = 78;
 	}
-		else if (spped > 1115){
+		else if (speed > 1115){
 		power = 77;
 	}
 	else if (speed > 1100){
 		power = 76;
 	}
-		else if (spped > 1075){
+		else if (speed > 1075){
 		power = 75;
 	}
 	else if (speed > 1050){
 		power = 74;
 	}
-		else if (spped > 1040){
+		else if (speed > 1040){
 		power = 72;
 	}
 	else if (speed > 1030){
 		power = 70;
 	}
-		else if (spped > 1015){
+		else if (speed > 1015){
 		power = 69;
 	}
 	else if (speed > 1000){
 		power = 68;
 	}
-		else if (spped > 983){
+		else if (speed > 983){
 		power = 67;
 	}
 	else if (speed > 966){
 		power = 66;
 	}
-		else if (spped > 948){
+		else if (speed > 948){
 		power = 65;
 	}
 	else if (speed > 933){
 		power = 64;
 	}
-		else if (spped > 915){
+		else if (speed > 915){
 		power = 62;
 	}
 	else if (speed > 900){
 		power = 60;
 	}
-		else if (spped > 880){
+		else if (speed > 880){
 		power = 59;
 	}
 	else if (speed > 866){
 		power = 58;
 	}
-		else if (spped > 850){
+		else if (speed > 850){
 		power = 57;
 	}
 	else if (speed > 833){
 		power = 56;
 	}
-		else if (spped > 815){
+		else if (speed > 815){
 		power = 55;
 	}
 	else if (speed > 800){
 		power = 54;
 	}
-		else if (spped > 780){
+		else if (speed > 780){
 		power = 53;
 	}
 	else if (speed > 766){
 		power = 52;
 	}
-		else if (spped > 750){
+		else if (speed > 750){
 		power = 51;
 	}
 	else if (speed > 733){
 		power = 50;
 	}
-		else if (spped > 715){
+		else if (speed > 715){
 		power = 49;
 	}
 	else if (speed > 700){
 		power = 48;
 	}
-		else if (spped > 780){
+		else if (speed > 780){
 		power = 47;
 	}
 	else if (speed > 666){
 		power = 46;
 	}
-		else if (spped > 650){
+		else if (speed > 650){
 		power = 45;
 	}
 	else if (speed > 633){
 		power = 44;
 	}
-		else if (spped > 615){
+		else if (speed > 615){
 		power = 43;
 	}
 	else if (speed > 600){
