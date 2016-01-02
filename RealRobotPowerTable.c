964f0916746264b2017cc69d791c62c04054b40d
Thomas Andrasek <FabulousWalrus329@gmail.com>
2016-01-02 12:23:51 -0500

    Changed power values in ShooterPowerTable

--- RealRobotPowerTable.c
+++ RealRobotPowerTable.c
@@ -4,193 +4,193 @@ int getRightShooterPower(int speed){
 		power = 90;
 	}
 	else if (speed > 935){
-		power = 89;
-	}
-	else if (speed > 925){
-		power = 88;
-	}
-	else if (speed > 915){
-		power = 87;
-	}
-	else if (speed > 905){
-		power = 86;
-	}
-	else if (speed > 895){
-		power = 85;
-	}
-	else if (speed > 885){
-		power = 82;
-	}
-	else if (speed > 880){
-		power = 80;
-	}
-	else if (speed > 870){
-		power = 79;
-	}
-	else if (speed > 865){
-		power = 78;
-	}
-	else if (speed > 860){
-		power = 77;
-	}
-	else if (speed > 855){
-		power = 76;
-	}
-	else if (speed > 850){
-		power = 75;
-	}
-	else if (speed > 848){
-		power = 74;
-	}
-	else if (speed > 846){
-		power = 73;
-	}
-	else if (speed > 844){
-		power = 72;
-	}
-	else if (speed > 842){
-		power = 71;
-	}
-	else if (speed > 840){
 		power = 70;
 	}
-	else if (speed > 835){
+	else if (speed > 925){
 		power = 69;
 	}
-	else if (speed > 830){
+	else if (speed > 915){
 		power = 68;
 	}
-	else if (speed > 820){
+	else if (speed > 905){
+		power = 67;
+	}
+	else if (speed > 895){
 		power = 66;
 	}
-	else if (speed > 810){
+	else if (speed > 885){
 		power = 65;
 	}
-	else if (speed > 805){
+	else if (speed > 880){
 		power = 64;
 	}
-	else if (speed > 800){
+	else if (speed > 870){
 		power = 63;
 	}
-	else if (speed > 790){
+	else if (speed > 865){
 		power = 62;
 	}
-	else if (speed > 780){
+	else if (speed > 860){
 		power = 61;
 	}
-	else if (speed > 770){
+	else if (speed > 855){
 		power = 60;
 	}
-	else if (speed > 760){
+	else if (speed > 850){
+		power = 59;
+	}
+	else if (speed > 848){
 		power = 58;
 	}
-		else if (speed > 750){
+	else if (speed > 846){
 		power = 57;
 	}
-	else if (speed > 747){
+	else if (speed > 844){
 		power = 56;
 	}
-	else if (speed > 745){
+	else if (speed > 842){
 		power = 55;
 	}
-		else if (speed > 730){
+	else if (speed > 840){
 		power = 54;
 	}
-	else if (speed > 710){
+	else if (speed > 835){
+		power = 53;
+	}
+	else if (speed > 830){
 		power = 52;
 	}
-		else if (speed > 690){
+	else if (speed > 820){
 		power = 51;
 	}
-	else if (speed > 670){
+	else if (speed > 810){
 		power = 50;
 	}
-		else if (speed > 665){
+	else if (speed > 805){
 		power = 49;
 	}
-	else if (speed > 660){
+	else if (speed > 800){
 		power = 48;
 	}
-		else if (speed > 650){
+	else if (speed > 790){
 		power = 47;
 	}
-	else if (speed > 640){
+	else if (speed > 780){
 		power = 46;
 	}
-	else if (speed > 630){
+	else if (speed > 770){
 		power = 45;
 	}
-	else if (speed > 620){
+	else if (speed > 760){
 		power = 44;
 	}
-	else if (speed > 610){
-		power = 44;
-	}
-	else if (speed > 600){
+		else if (speed > 750){
 		power = 43;
 	}
-	else if (speed > 590){
+	else if (speed > 747){
 		power = 42;
 	}
-	else if (speed > 580){
+	else if (speed > 745){
 		power = 41;
 	}
-	else if (speed > 570){
+		else if (speed > 730){
 		power = 40;
 	}
-	else if (speed > 560){
+	else if (speed > 710){
 		power = 39;
 	}
-	else if (speed > 550){
+		else if (speed > 690){
 		power = 38;
 	}
-	else if (speed > 540){
+	else if (speed > 670){
 		power = 37;
 	}
-	else if (speed > 530){
+		else if (speed > 665){
 		power = 36;
 	}
-	else if (speed > 520){
+	else if (speed > 660){
 		power = 35;
 	}
-	else if (speed > 510){
+		else if (speed > 650){
 		power = 34;
 	}
-	else if (speed > 500){
+	else if (speed > 640){
 		power = 33;
 	}
-	else if (speed > 490){
+	else if (speed > 630){
 		power = 32;
 	}
-	else if (speed > 480){
+	else if (speed > 620){
 		power = 31;
 	}
-	else if (speed > 470){
+	else if (speed > 610){
 		power = 30;
 	}
-	else if (speed > 460){
+	else if (speed > 600){
 		power = 29;
 	}
-	else if (speed > 450){
+	else if (speed > 590){
 		power = 28;
 	}
-	else if (speed > 440){
+	else if (speed > 580){
 		power = 27;
 	}
-	else if (speed > 430){
+	else if (speed > 570){
 		power = 26;
 	}
-	else if (speed > 420){
+	else if (speed > 560){
 		power = 25;
 	}
-	else if (speed > 410){
+	else if (speed > 550){
 		power = 24;
 	}
-	else if (speed > 400){
+	else if (speed > 540){
 		power = 23;
 	}
-	else if (speed > 100){
+	else if (speed > 530){
+		power = 22;
+	}
+	else if (speed > 520){
+		power = 21;
+	}
+	else if (speed > 510){
 		power = 20;
+	}
+	else if (speed > 500){
+		power = 19;
+	}
+	else if (speed > 490){
+		power = 18;
+	}
+	else if (speed > 480){
+		power = 17;
+	}
+	else if (speed > 470){
+		power = 16;
+	}
+	else if (speed > 460){
+		power = 15;
+	}
+	else if (speed > 450){
+		power = 14;
+	}
+	else if (speed > 440){
+		power = 13;
+	}
+	else if (speed > 430){
+		power = 12;
+	}
+	else if (speed > 420){
+		power = 11;
+	}
+	else if (speed > 410){
+		power = 10;
+	}
+	else if (speed > 400){
+		power = 9;
+	}
+	else if (speed > 100){
+		power = 5;
 	}
 	else {
 		power = 0;
@@ -202,180 +202,180 @@ int getRightShooterPower(int speed){
 int getLeftShooterPower(int speed){
 	int power = 0;
   if (speed > 975){
-		power = 90;
+		power = 70;
 	}
 	else if (speed > 955){
-		power = 89;
+		power = 69;
 	}
 	else if (speed > 935){
-		power = 87;
+		power = 68;
 	}
 	else if (speed > 915){
-		power = 86;
+		power = 67;
 	}
 	else if (speed > 895){
-		power = 85;
+		power = 66;
 	}
 
 	else if (speed > 875){
-		power = 83;
+		power = 65;
 	}
 	else if (speed > 870){
-		power = 82;
+		power = 64;
 	}
 	else if (speed > 865){
-		power = 81;
+		power = 63;
 	}
 	else if (speed > 860){
-		power = 80;
+		power = 62;
 	}
 
 	else if (speed > 854){
-		power = 79;
+		power = 61;
 	}
 	else if (speed > 848){
-		power = 78;
-	}
-		else if (speed > 842){
-		power = 77;
-	}
-	else if (speed > 836){
-		power = 76;
-	}
-		else if (speed > 830){
-		power = 75;
-	}
-	else if (speed > 820){
-		power = 74;
-	}
-		else if (speed > 810){
-		power = 72;
-	}
-	else if (speed > 800){
-		power = 70;
-	}
-		else if (speed > 794){
-		power = 69;
-	}
-	else if (speed > 788){
-		power = 68;
-	}
-		else if (speed > 782){
-		power = 67;
-	}
-	else if (speed > 776){
-		power = 66;
-	}
-		else if (speed > 770){
-		power = 65;
-	}
-	else if (speed > 755){
-		power = 64;
-	}
-		else if (speed > 745){
-		power = 62;
-	}
-	else if (speed > 730){
 		power = 60;
 	}
-		else if (speed > 722){
+		else if (speed > 842){
 		power = 59;
 	}
-	else if (speed > 714){
+	else if (speed > 836){
 		power = 58;
 	}
-		else if (speed > 706){
+		else if (speed > 830){
 		power = 57;
 	}
-	else if (speed > 698){
+	else if (speed > 820){
 		power = 56;
 	}
-		else if (speed > 690){
+		else if (speed > 810){
 		power = 55;
 	}
-	else if (speed > 680){
+	else if (speed > 800){
 		power = 54;
 	}
-		else if (speed > 655){
+		else if (speed > 794){
 		power = 53;
 	}
-	else if (speed > 640){
+	else if (speed > 788){
 		power = 52;
 	}
-		else if (speed > 625){
+		else if (speed > 782){
 		power = 51;
 	}
-	else if (speed > 620){
+	else if (speed > 776){
 		power = 50;
 	}
-		else if (speed > 610){
+		else if (speed > 770){
 		power = 49;
 	}
-	else if (speed > 600){
+	else if (speed > 755){
 		power = 48;
 	}
-		else if (speed > 590){
+		else if (speed > 745){
 		power = 47;
 	}
-	else if (speed > 580){
+	else if (speed > 730){
 		power = 46;
 	}
-		else if (speed > 570){
+		else if (speed > 722){
 		power = 45;
 	}
-	else if (speed > 560){
+	else if (speed > 714){
 		power = 44;
 	}
-	else if (speed > 550){
+		else if (speed > 706){
 		power = 43;
 	}
-	else if (speed > 540){
+	else if (speed > 698){
 		power = 42;
 	}
-	else if (speed > 530){
+		else if (speed > 690){
 		power = 41;
 	}
-	else if (speed > 520){
+	else if (speed > 680){
 		power = 40;
 	}
-	else if (speed > 510){
+		else if (speed > 655){
 		power = 39;
 	}
-	else if (speed > 500){
-		power = 39;
-	}
-	else if (speed > 490){
+	else if (speed > 640){
 		power = 38;
 	}
-	else if (speed > 480){
+		else if (speed > 625){
 		power = 37;
 	}
-	else if (speed > 470){
+	else if (speed > 620){
 		power = 36;
 	}
-	else if (speed > 460){
+		else if (speed > 610){
 		power = 35;
 	}
-	else if (speed > 450){
+	else if (speed > 600){
 		power = 34;
 	}
-	else if (speed > 440){
+		else if (speed > 590){
 		power = 33;
 	}
-	else if (speed > 430){
+	else if (speed > 580){
 		power = 32;
 	}
-	else if (speed > 420){
+		else if (speed > 570){
 		power = 31;
 	}
-	else if (speed > 410){
+	else if (speed > 560){
 		power = 30;
 	}
-	else if (speed > 400){
+	else if (speed > 550){
 		power = 29;
 	}
-	else if (speed > 100){
+	else if (speed > 540){
+		power = 28;
+	}
+	else if (speed > 530){
+		power = 27;
+	}
+	else if (speed > 520){
+		power = 26;
+	}
+	else if (speed > 510){
+		power = 25;
+	}
+	else if (speed > 500){
+		power = 24;
+	}
+	else if (speed > 490){
+		power = 23;
+	}
+	else if (speed > 480){
+		power = 22;
+	}
+	else if (speed > 470){
+		power = 21;
+	}
+	else if (speed > 460){
 		power = 20;
+	}
+	else if (speed > 450){
+		power = 19;
+	}
+	else if (speed > 440){
+		power = 18;
+	}
+	else if (speed > 430){
+		power = 17;
+	}
+	else if (speed > 420){
+		power = 16;
+	}
+	else if (speed > 410){
+		power = 15;
+	}
+	else if (speed > 400){
+		power = 14;
+	}
+	else if (speed > 100){
+		power = 5;
 	}
 	else {
 		power = 0;
