5518750f19cfc57fd9b869c176fc0243714515dc
Thomas Andrasek <FabulousWalrus329@gmail.com>
2016-01-02 11:44:25 -0500

    Created a Button for Lifting. Also added a ball backout button.

--- DriverJoystickControls.c
+++ DriverJoystickControls.c
@@ -13,12 +13,12 @@ task controllerPolling(){
 		}
 		else if((vexRT[Btn8R] == 1) || (vexRT[Btn8RXmtr2] == 1)){
 			// Set motor target speed (Medium), Channel 8, Button R
-			set_shooter_targets(770);
+			set_shooter_targets(780);
 			delay(500);
 		}
 		else if((vexRT[Btn8D] == 1) || (vexRT[Btn8DXmtr2] == 1)){
 			// Set motor target speed (Short), Channel 8, Button D
-			set_shooter_targets(570);
+			set_shooter_targets(620);
 			delay(500);
 		}
 		else if((vexRT[Btn6U] == 1) || (vexRT[Btn6UXmtr2] == 1)){
@@ -29,12 +29,18 @@ task controllerPolling(){
 		else if((vexRT[Btn6D] == 1) || (vexRT[Btn6DXmtr2] == 1)){
 			// Adjust motor target speed (5 down), Channel 6, Button D
 			adjust_shooter_targets(-10);
-			delay(250);
+			delay(500);
 		}
 		else if((vexRT[Btn8L] == 1) || (vexRT[Btn8LXmtr2] == 1)){
 			// Stop all motor speed (0), Channel 8, Button L
+			if (shooter_target_speed == 0){
+				backOut = true;
+			}
 			set_shooter_targets(0);
-			delay(250);
+			delay(500);
+		}
+		else if (backOut){
+			backOut = false;
 		}
 		else if((vexRT[Btn7U] == 1) || (vexRT[Btn7UXmtr2] == 1)){
 			// Forward Intake, Channel 7, Button U
@@ -125,9 +131,22 @@ task driving(){
 				motor[leftIntake] = 0;
 			}
 		}
+
+		// Lifter controls
+
+		if((vexRT[Btn5U] == 1) || (vexRT[Btn5UXmtr2] == 1)){
+			motor[rightLifter] = 80;
+			motor[leftLifter] = 80;
+		}
+		else if((vexRT[Btn5D] == 1) || (vexRT[Btn5UXmtr2] == 1)){
+			motor[rightLifter] = -80;
+			motor[leftLifter] = -80;
+		}
+		else {
+			motor[rightLifter] = 0;
+			motor[leftLifter] = 0;
+		}
 		delay(50);
 	}
 }
-
-
 // End Driver joystick Controls
